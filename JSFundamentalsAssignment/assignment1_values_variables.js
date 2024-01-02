362f4f6d15a69af6fd75d09ebab65e8bb700c723
Lucas Leow <leowjianhoong@gmail.com>
2024-01-03 00:24:19 +0800

    complete JSFundamentals assignment 1

---
+++ JSFundamentalsAssignment/assignment1_values_variables.js
@@ -0,0 +1,5 @@
+let country = 'RepublicNation';
+let continent = 'RepublicContinent';
+let population = 1000000;
+
+console.log(`Country: ${country} | Continent: ${continent} | Population: ${population}`);
