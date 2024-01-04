c0b5fa76d4e72e4b7149d54fd4e9c116319fff0e
Lucas Leow <leowjianhoong@gmail.com>
2024-01-04 21:01:10 +0800

    Complete first practice

--- JSFundamentalsAssignment/assignment1_values_variables.js
+++ JSFundamentalsAssignment/assignment1_values_variables.js
@@ -1,5 +1,21 @@
-let country = 'RepublicNation';
-let continent = 'RepublicContinent';
+// Values and Variables Practice
+const country = 'RepublicNation';
+const continent = 'RepublicContinent';
 let population = 1000000;
 
 console.log(`Country: ${country} | Continent: ${continent} | Population: ${population}`);
+
+// Data Types Practice
+const isIsland = true;
+let language = 'English';
+
+console.log(isIsland, population, country, language);
+
+// Basic Operator practice
+let country_in_half = population / 2;
+console.log(country_in_half);
+
+console.log(population + 1)
+
+let finlandPopulation = 6000000;
+console.log(`My country population more than Finalnd?: ${population > finlandPopulation}`);
