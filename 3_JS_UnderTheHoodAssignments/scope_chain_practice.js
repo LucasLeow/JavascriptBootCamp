2dcbec36879cd083385320dca7b5d3e5c2191410
Lucas Leow <leowjianhoong@gmail.com>
2024-02-05 21:13:00 +0800

    Complete scope chain practice

---
+++ 3_JS_UnderTheHoodAssignments/scope_chain_practice.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
+  console.log(firstName); // firstName is global scoped
+
+  // defined inside calcAge, cannot be accessed in global scope
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`; // age & birthYear parent variable.
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const blockScopeVar = `You are a millenial`; // let & const can be block-scoped.
+      console.log(blockScopeVar);
+
+      // function add is block-scoped if use strict mode is used
+      function add(a, b) {
+        return a + b;
+      }
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Lucas';
+calcAge(1995);
