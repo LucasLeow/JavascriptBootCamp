25b80a8987e0ab8b69c25386a7a3e47224bd6e0a
Lucas Leow <leowjianhoong@gmail.com>
2024-02-13 17:35:09 +0800

    Complete Functions Code Challenge 1

---
+++ 5_JS_Functions/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    let qns_str = '';
+    for (let qns of poll.options) {
+      qns_str += qns + '\n';
+    }
+    let user_input = Number(prompt(`${this.question}\n${qns_str}`));
+    user_input >= 0 && user_input <= 3
+      ? (this.answers[user_input] += 1)
+      : alert('Invalid choice, please choose between 0 and 4');
+    this.displayResults('string');
+  },
+  displayResults(type) {
+    if (type === 'array') {
+      console.log(`${this.answers}`);
+    } else if (type === 'string') {
+      let answer_string = this.answers.join(', ');
+      console.log(`Poll results are ${answer_string}`);
+    }
+  },
+};
+
+let ans_poll_btn_node = document.querySelector('.poll');
+ans_poll_btn_node.addEventListener('click', poll.registerNewAnswer.bind(poll));
