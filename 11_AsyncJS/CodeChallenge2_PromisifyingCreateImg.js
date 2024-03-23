db36ca66851470df05cd90b2fc8ad8eb1ad28ded
LucasLeow <leowjianhoong@gmail.com>
2024-03-23 14:10:16 +0800

    Complete Async JS Code Challenge 2

--- 11_AsyncJS/CodeChallenge2_PromisifyingCreateImg.js
+++ 11_AsyncJS/CodeChallenge2_PromisifyingCreateImg.js
@@ -1,9 +1,16 @@
 'use strict';
+let newImg;
+
+const wait = function (seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
 const createImage = function (imgPath) {
   return new Promise(function (resolve, reject) {
     if (!imgPath) reject('No img URL provided');
-    let newImg = document.createElement('img');
+    newImg = document.createElement('img');
     newImg.setAttribute('src', imgPath);
     newImg.addEventListener('load', () => {
       document
@@ -17,5 +24,19 @@ const createImage = function (imgPath) {
 createImage(
   'https://yt3.googleusercontent.com/ytc/AIdro_k2wsQa2j9sAhjS25DyZxrhAGDJWtNZBYcLVd3uqQ=s900-c-k-c0x00ffffff-no-rj'
 )
-  .then(res => console.log('here', res))
+  .then(imgNode => console.log(imgNode))
   .catch(err => console.log(err));
+
+wait(3)
+  .then(() => {
+    newImg.style.display = 'none';
+    return createImage(
+      'https://raw.githubusercontent.com/HyunCafe/HyunCafe/main/assests/loficity.gif'
+    );
+  })
+  .then(imgNode => {
+    return wait(3);
+  })
+  .then(() => {
+    newImg.style.display = 'none';
+  });
