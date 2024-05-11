31c1161707cafa7437de973cbc3a4be1b5e7118b
Lucas Leow <leowjianhoong@gmail.com>
2024-05-11 14:09:38 +0800

    Implement Spinner while loading data

--- 12_ForkifyApp/src/js/controller.js
+++ 12_ForkifyApp/src/js/controller.js
@@ -14,9 +14,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentElement) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+  parentElement.innerHTML = ''; // clear existing content
+  parentElement.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
     // Loading Recipe
+    renderSpinner(recipeContainer);
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
@@ -93,7 +106,6 @@ const showRecipe = async function () {
       <ul class="recipe__ingredient-list">
         ${recipe.ingredients
           .map(ing => {
-            console.log(ing);
             return `
               <li class="recipe__ingredient">
               <svg class="recipe__icon">
